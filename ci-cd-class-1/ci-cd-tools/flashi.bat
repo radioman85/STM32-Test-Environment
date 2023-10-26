a64fce3fbd674d3361cae24a2d267ecab3dfc74a
Niklas Roth <niklas.roth@noser.com>
2023-10-26 14:27:16 +0200

    updated flash script to fit my projects path.

--- ci-cd-class-1/ci-cd-tools/flashi.bat
+++ ci-cd-class-1/ci-cd-tools/flashi.bat
@@ -6,8 +6,8 @@ setlocal
 set "build_type=Debug"
 if not [%1]==[] set "build_type=%1"
 
-set "ws_root=C:\Users\gene\Documents\Gene\proj\STM32CubeIDE\workspace_1.6.1\ci-cd-class-1"
-set "sn=066BFF575550897767162155"
+set "ws_root=C:\98-Others\STM32-Test-Environment\ci-cd-class-1"
+set "sn=066BFF554852707267081044"
 set "image_file=%ws_root%\%build_type%\ci-cd-class-1.bin"
 
 "%ws_root%\ci-cd-tools\flash.bat" %sn% "%image_file%"
